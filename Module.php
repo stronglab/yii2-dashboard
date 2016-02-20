8f1a31ce2be44a026fb4369a86fe62982928aefd
strong <strong.barnaul@gmail.com>
2016-02-20 23:41:41 +0300

    Hello World

---
+++ Module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace stronglab\yii2\dashboard;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+
+/**
+ * @author strong
+ */
+class Module extends \yii\base\Module {
+    
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace stronglab\yii2\dashboard\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * DefaultController
+ *
+ * @author strong
+ */
+class DefaultController extends Controller {
+
+    public function actionIndex() {
+        return 'Hello World!!!';
+    }
+
+}
